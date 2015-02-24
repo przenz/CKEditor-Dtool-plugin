49429063889e6a7224a2d9b533a1603c2a8aa85a
przenz <przenziu@gmail.com>
2015-02-24 22:21:17 +0100

    Added Timestamp and Show/Hide functions

--- dtool-assets/showhide.js
+++ dtool-assets/showhide.js
@@ -1,26 +1,24 @@
 function switchShowHide() {
   var showHideList = document.getElementsByClassName("showhide-switch");
   var offset = "15";
+  var docWidth = document.body.clientWidth;
+  
   for (var i = 0; i < showHideList.length; i++) {
     showHideList[i].onclick = function() {
+      var parentNode = this.parentNode;
       if( this.value == '+ Show') {
         this.value = '- Hide';
-        this.parentNode.className += " showhide-open";
-        this.style.width = "100%";
+        this.className += " showhide-switch-open";
+        parentNode.className += " showhide-open";
         
-        var docWidth = document.body.clientWidth;
-        var showHideRect = this.parentNode.getBoundingClientRect();
-        var showHideWidth = this.parentNode.offsetWidth;
-        
-        if( showHideRect.right > docWidth ) {
-          var scrollVal = document.documentElement.scrollLeft;
-          this.parentNode.style.left = docWidth - showHideWidth - offset + scrollVal + "px";
+        if (parentNode.getBoundingClientRect().right > docWidth) {
+            parentNode.style.left = docWidth - parentNode.offsetWidth - offset + document.documentElement.scrollLeft + 'px';
         }
       } else {
         this.value = '+ Show';
-        this.parentNode.className = "showhide";
-        this.style.width = "";
-        this.parentNode.style.left = "auto";
+        this.className = " showhide-switch";
+        parentNode.className = "showhide";
+        parentNode.style.left = "auto";
       }
     };
   }
--- plugin.js
+++ plugin.js
@@ -1,6 +1,20 @@
 CKEDITOR.plugins.add( 'dtool',{
     init: function( editor ) {
       var pluginDirectory = this.path;
+      
+      // Editor mode change for Showhide switch hooks refresh
+      editor.on( 'mode', function() {
+        if ( this.mode != 'wysiwyg' )
+          return;
+        
+        var elements = editor.document.find( ".showhide-switch" );
+        for ( var i = 0; i < elements.count(); ++i ) {
+          elements.getItem(i).on( 'click', function( event ) {
+            switcherShowHide( event.sender );
+          });
+        }
+      });
+      
       // Verification template
       editor.addCommand( 'dtool-verifTemplate', {
         exec: function( editor ) {
@@ -15,9 +29,17 @@ CKEDITOR.plugins.add( 'dtool',{
                 <br />CORRECTION VERIFIED BY \
                 <br />------------------------------------------ \
                 <br />Running FT Testcase(s): \
-                <br /> ' );
+                <br /> \
+                <br />ADDITIONAL VERIFICATION NEEDED \
+                <br />------------------------------------------ \
+                <br />No \
+                <br /> \
+                <br />IMPACTED DOCUMENTS (FTD, DR, CPI etc) \
+                <br />------------------------------------------ \
+                <br />N/A ' );
         }
       });
+      
       editor.ui.addButton( 'dtool-verifTemplate', {
         label: 'Verification info',
         command: 'dtool-verifTemplate',
@@ -26,16 +48,39 @@ CKEDITOR.plugins.add( 'dtool',{
       });
       
       // Show/Hide
+      function switcherShowHide( sender ) {
+        var parent = sender.getParent();
+        if ( parent.hasClass( "showhide-open" ) ) { // Hiding
+          parent.removeClass( "showhide-open" );
+          sender.setValue("+ Show");
+          sender.removeClass( "showhide-switch-open" );
+        } else { // Showing
+          parent.addClass( "showhide-open" );
+          sender.setValue("- Hide");
+          sender.addClass( "showhide-switch-open" );
+        }
+      }
+
+      function createShowHide() {
+        var showHideWrap = editor.document.createElement( 'div', { 'attributes' : { 'class': 'showhide-wrap' } } );
+        var showHide = editor.document.createElement( 'div', { 'attributes' : { 'class': 'showhide showhide-open' } } );
+        var showHideSwitch = editor.document.createElement( 'input', 
+                             { 'attributes' : { 'class': 'showhide-switch showhide-switch-open', 'type': 'button', 'value': '- Hide' } } );
+        showHideSwitch.on( 'click', function( event ) {
+          switcherShowHide( event.sender );
+        });
+        
+        showHide.append( showHideSwitch );
+        showHide.appendHtml( '<br />Place your hidden content here.<br />' );
+        showHideWrap.append( showHide );
+        return showHideWrap;
+      }
+      
       editor.addCommand( 'dtool-showHide', {
         exec: function( editor ) {
-          var showHideContent = "";
-          editor.insertHtml( ' \
-              <br /><div class="showhide-wrap"> \
-                <div class="showhide"> \
-                  <input type="button" value="+ Show" class="showhide-switch"/><br /> \
-                  <br /> \
-                </div> \
-              </div><br />' );
+          var showHide = createShowHide();
+          editor.insertElement( showHide );
+          editor.insertHtml( "<br />" );
         },
         allowedContent: 'div(showhide*);input(*)[*]'
       });
@@ -43,6 +88,34 @@ CKEDITOR.plugins.add( 'dtool',{
         label: 'Show/Hide',
         command: 'dtool-showHide',
         icon: pluginDirectory + 'showhide.png',
+        toolbar: 'insert'
+      });
+      
+      // Timestamp
+      editor.addCommand( 'dtool-timestamp', {
+        exec: function( editor ) {
+          var d = new Date();
+          var curr_day = d.getDate();
+          if( curr_day < 10 ) {
+            curr_day = "0" + curr_day;
+          }
+          var curr_month = d.getMonth()+1;
+          if( curr_month < 10 ) {
+            curr_month = "0" + curr_month;
+          }
+          var curr_year = d.getFullYear();
+          var date = curr_year +"-"+ curr_month +"-"+ curr_day;
+
+          var getSignum = document.querySelectorAll(".logout > strong");
+          var signum = getSignum[0].innerHTML.substring(0, 7);
+
+          editor.insertHtml( "<br />&#8226<strong>" + date + "</strong> (" + signum + "): " );
+        }
+      });
+      editor.ui.addButton( 'dtool-timestamp', {
+        label: 'Timestamp',
+        command: 'dtool-timestamp',
+        icon: pluginDirectory + 'timestamp.png',
         toolbar: 'insert'
       });
       
